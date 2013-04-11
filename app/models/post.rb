802a9c2497b2215ed2a43b5f12696a7fc290b037
Joe Mahoney <jmahoney@iterative.co.nz>
2013-04-12 08:36:10 +1200

    automagically generate slugs with stringex

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
+  acts_as_url :title, :url_attribute => :slug, :only_when_blank => true
+  validates_presence_of :title, :content, :published_at, :slug
+  
   def self.available_statuses
     %w(draft live foo bar)
   end
+  
+    
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,4 +5,34 @@ class PostTest < ActiveSupport::TestCase
     assert_equal('draft', Post.new.status)
   end
   
+  test "requires title, content, published_at" do
+    p = Post.new
+    assert !p.save
+    
+    p.title = "foo"
+    assert !p.save
+    
+    p.content = "bar"
+    assert !p.save
+    
+    p.published_at = DateTime.now
+    assert p.save
+  end
+  
+  test "creates slug from title when slug is blank" do
+    p = Post.new(:title => 'This is my post', :content => 'bar', :published_at => DateTime.now)
+    p.save
+    assert_equal("this-is-my-post", p.slug, "generated slug is incorrect")
+  end
+  
+  test "does not automagically create slug when already populated" do
+    p = Post.new(:title => 'This is my post', :content => 'bar', :slug => 'test-post', :published_at => DateTime.now)
+    p.save
+    assert_equal("test-post", p.slug, "found a generated slug when a manual one was specified")
+  end
+  
+  
+  
+  
+  
 end
