c38b532599e983beeeba0f2ce4902f590e432e13
Joe Mahoney <jmahoney@iterative.co.nz>
2013-05-21 21:32:41 +1200

    links to monthly archives

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,14 @@ module PostsHelper
   def month_archive_link(item)
     url = "/#{item.year.to_i}/#{item.month.to_i}"
     link_text = "#{Date::MONTHNAMES[item.month]}, #{item.year.to_i}"
-    link_to(link_text, url)
+    title_text = "#{item.post_count} #{posts_str(item.post_count)} in #{link_text}"
+    link_to(link_text, url, {title: title_text})
+  end
+  
+  
+  private 
+  #should we show "posts" or "post"? 
+  def posts_str(post_count)
+    post_count == 1 ? "post" : "posts"
   end
 end
