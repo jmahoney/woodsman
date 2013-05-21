96f9ff6bbeae976b0afa74b7880bd7df1ccdb30d
Joe Mahoney <jmahoney@iterative.co.nz>
2013-05-21 22:55:37 +1200

    padd zero to month in url

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   # create a link to a particular year/month in the blog archive
   def month_archive_link(item)
-    url = "/#{item.year.to_i}/#{item.month.to_i}"
+    url = "/#{item.year.to_i}/#{sprintf('%02d',item.month.to_i)}"
     link_text = "#{Date::MONTHNAMES[item.month]}, #{item.year.to_i}"
     title_text = "#{item.post_count} #{posts_str(item.post_count)} in #{link_text}"
     link_to(link_text, url, {title: title_text})
