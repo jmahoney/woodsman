856384bea3c7ab764f87bd77e486e4f2f78f4122
Joe Mahoney <jmahoney@iterative.co.nz>
2013-07-11 19:50:52 +1200

    fixing dumb mistake

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,8 +1,6 @@
 class Admin::AdminController < ApplicationController
   http_basic_authenticate_with :name => ENV['WOODSMAN_USER'], 
-                               :password => ENV['WOODSMAN_PASSWORD'], 
-                               :except => [:index, :show]
-                               
+                               :password => ENV['WOODSMAN_PASSWORD']                                                           
   respond_to :html
   layout "admin"
   
