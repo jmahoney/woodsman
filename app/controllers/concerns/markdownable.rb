16887c685d6f735d41917b799cc2d7f562a3d2e5
Joe Mahoney <jmahoney@iterative.co.nz>
2014-08-31 05:49:49 +1200

    smarter pants

--- app/controllers/concerns/markdownable.rb
+++ app/controllers/concerns/markdownable.rb
@@ -1,20 +1,25 @@
 module Markdownable
   extend ActiveSupport::Concern
-  
   included do
     before_action :set_markdown_renderer
   end
-  
+
   # set up a renderer that the views can use to render markdown
   # stored in i18n files and post content
   def set_markdown_renderer
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
-                    :autolink => true, 
+    @markdown = Redcarpet::Markdown.new(Pants,
+                    :autolink => true,
                     :disable_indented_code_blocks => true,
                     :fenced_code_blocks => true,
                     :lax_spacing => true,
                     :no_intra_emphasis => true,
                     :space_after_headers => true,
                     :tables => true)
+
   end
+end
+
+class Pants < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+
 end
